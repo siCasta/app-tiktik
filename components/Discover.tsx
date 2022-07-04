a91025ff3b3c33a689ba4478dc8e993f7d84d0aa
siCasta <sicastaprojectos@gmail.com>
2022-07-04 00:37:01 -0500

    First commit

---
+++ components/Discover.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import { topics } from '../utils/constants'
+
+const Discover = () => {
+    return (
+        <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
+            <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
+                Popular Topics
+            </p>
+            <div className="flex gap-3 flex-wrap">
+                {topics.map(topic => (
+                    <Link href={`/?topic=${topic.name}`} key={topic.name}>
+                        <div>
+                            <span>
+                                {topic.icon}
+                            </span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default Discover
---
+++ components/SuggestedAcounts.tsx
@@ -0,0 +1,6 @@
+const SuggestedAcounts = () => {
+    return (
+        <div>SuggestedAcounts</div>
+    )
+}
+export default SuggestedAcounts
